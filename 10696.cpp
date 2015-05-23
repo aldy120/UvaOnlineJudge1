95b21bfeac3397cdea3c6e4cbe1cad54c879f05d
aldy120 <aldy120@yahoo.com.tw>
2015-05-24 02:00:51 +0800

    Create 10696.cpp

---
+++ 10696.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : F91.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<cstdio>
+int f91(int n)
+{
+	if(n<=100)
+		return f91(f91(n+11));
+	else
+		return n-10;
+}
+int main()
+{
+	int nn;
+	while(scanf("%d",&nn)==1&&nn)
+	{
+		printf("f91(%d) = %d\n",nn,f91(nn));
+	}
+	return 0;
+}
